845e13de8a396842b6323da58fed91fcf108b5f1
111306047 <118511796+111306047@users.noreply.github.com>
2024-05-07 01:48:15 +0800

    change

--- backend/houseRentingPlatform/src/main/java/com/goHome/houseRentingPlatform/controller/ArticleController.java
+++ backend/houseRentingPlatform/src/main/java/com/goHome/houseRentingPlatform/controller/ArticleController.java
@@ -2,24 +2,35 @@ package com.goHome.houseRentingPlatform.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.goHome.houseRentingPlatform.model.Article;
 import com.goHome.houseRentingPlatform.model.Article.ArticleType;
 import com.goHome.houseRentingPlatform.service.ArticleService;
-
+import com.goHome.houseRentingPlatform.service.CommentService;
 @RestController
 public class ArticleController {
 
     private final ArticleService articleService;
+     private final CommentService commentService;
 
-    public ArticleController(ArticleService articleService) {
+    public ArticleController(ArticleService articleService, CommentService commentService) {
         this.articleService = articleService;
+        this.commentService = commentService;
     }
 
     @GetMapping("/articles/filter")
     public List<Article> getFilteredArticles(@RequestParam ArticleType type) {
         return articleService.getArticlesByType(type);
+    }
+
+    @PostMapping("/articles/{articleId}/comments")
+    public void addComment(@PathVariable Long articleId,
+                           @RequestParam Long userId,
+                           @RequestParam String content) {
+        commentService.addComment(articleId, userId, content);
     }
 }
