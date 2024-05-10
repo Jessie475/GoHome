02883a5384fb9521012d87dc44044b90d3f80791
林慶琳 <118511796+111306047@users.noreply.github.com>
2024-05-10 18:45:07 +0800

    Update ArticleRepository.java

--- backend/houseRentingPlatform/src/main/java/com/goHome/houseRentingPlatform/repository/ArticleRepository.java
+++ backend/houseRentingPlatform/src/main/java/com/goHome/houseRentingPlatform/repository/ArticleRepository.java
@@ -1,4 +1,5 @@
 package com.goHome.houseRentingPlatform.repository;
+
 import java.util.List;
 
 import org.springframework.data.domain.Page;
@@ -9,20 +10,14 @@ import org.springframework.data.jpa.repository.Query;
 import com.goHome.houseRentingPlatform.model.Article;
 import com.goHome.houseRentingPlatform.model.Article.ArticleType;
 
-
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-    List<Article> findByTag(ArticleType tag);
-    List<Article> findByRateBetween(Double minRate, Double maxRate);
+    List<Article> findByType(ArticleType type);
 
-    @Query("SELECT a FROM Article a ORDER BY a.createdAt DESC")
+    List<Article> findByTypeAndRateBetween(ArticleType type, Double minRate, Double maxRate);
+
     Page<Article> findAllByOrderByCreatedAtDesc(Pageable pageable);
 
-    @Query("SELECT a, count(c.id) as commentCount FROM Article a LEFT JOIN Comment c ON a.id = c.article.id GROUP BY a.id ORDER BY commentCount DESC")
+    @Query("SELECT a FROM Article a LEFT JOIN a.comments c GROUP BY a ORDER BY COUNT(c) DESC")
     Page<Article> findAllByCommentCountDesc(Pageable pageable);
 
-    @Query("SELECT a FROM Article a ORDER BY a.rate DESC")
-    Page<Article> findAllByRateDesc(Pageable pageable);
-
-    @Query("SELECT a, count(f.id) as favoriteCount FROM Article a LEFT JOIN Favorite f ON a.id = f.article.id GROUP BY a.id ORDER BY favoriteCount DESC")
-    Page<Article> findAllByFavoriteCountDesc(Pageable pageable);
 }
