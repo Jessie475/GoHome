ffccfc1eb25eb234e5ba68534297a007fd040bb4
林慶琳 <118511796+111306047@users.noreply.github.com>
2024-05-10 19:07:33 +0800

    Update ArticleRepository.java

--- backend/houseRentingPlatform/src/main/java/com/goHome/houseRentingPlatform/repository/ArticleRepository.java
+++ backend/houseRentingPlatform/src/main/java/com/goHome/houseRentingPlatform/repository/ArticleRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.goHome.houseRentingPlatform.model.Article;
 import com.goHome.houseRentingPlatform.model.Article.ArticleType;
@@ -19,5 +20,8 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     @Query("SELECT a FROM Article a LEFT JOIN a.comments c GROUP BY a ORDER BY COUNT(c) DESC")
     Page<Article> findAllByCommentCountDesc(Pageable pageable);
+
+    @Query("SELECT a FROM Article a WHERE a.address LIKE %:address%")
+    List<Article> findByAddressContaining(@Param("address") String address);
 
 }
