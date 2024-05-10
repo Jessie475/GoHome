96d3de0a0a893961dca2842d76442f5770068641
Jessie475 <118877744+Jessie475@users.noreply.github.com>
2024-05-10 21:33:46 +0800

    Revert "change comment to class"
    This reverts commit cea8a8d58dc9e6717fef1f5427a65497ab66a081.

--- backend/houseRentingPlatform/src/main/java/com/goHome/houseRentingPlatform/model/Article.java
+++ backend/houseRentingPlatform/src/main/java/com/goHome/houseRentingPlatform/model/Article.java
@@ -16,7 +16,6 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-import org.hibernate.annotations.Comment;
 
 @Entity
 @Table(name = "article")
