0a4d9fd99b894ce7dd39fd010554985fdaf43a8b
Jessie475 <118877744+Jessie475@users.noreply.github.com>
2024-05-25 01:20:32 +0800

    upload frontend

---
+++ gohomefrontend/src/components/House.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/House.css'; 
+// 點地圖點會進來這一頁
+function House() {
+  return (
+    <h1></h1>
+  );
+}
+
+export default House;
---
+++ gohomefrontend/src/components/Login.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import '../css/Login.css'; // Import the CSS file
+
+const login = ({ username, password }) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (username === '1' && password === '1') {  // 帳號密碼
+        console.log(username, password);
+        resolve('success');
+      } else {
+        console.log(username, password);
+        reject('error');
+      }
+     }, 1000); //過1秒返回結果
+  });
+}
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const navigate = useNavigate();  
+
+  const handleSumbit = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    try {
+      await login({ username, password });
+      setIsSubmitting(false);
+      navigate('/home');  // 登錄成功後進入 Home 頁面
+    } catch (error) {
+      setError('incorrect username or password');
+      setUsername('');
+      setPassword('');
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-body">
+          {error && <h1 className="text-danger">{error}</h1>}
+          <form onSubmit={handleSumbit}>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.currentTarget.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.currentTarget.value)}
+                className="form-control"
+                id="password"
+              />
+            </div>
+            <div className='sign_up'> 
+              <Link className="link-style sign_up" to="/signup">註冊帳號</Link>              
+            </div>
+            <button type="submit" className="btn-block" disabled={isSubmitting}>
+              {isSubmitting ? 'Submitting...' : 'Submit'}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
